3cb3a5c9453b6d4dbee588d5f446b09b3ba7c853
Giovanni Zorzetto Oliveira <gigiozetto@gmail.com>
2024-06-13 16:52:44 -0300

    commit poetry

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from validate_docbr import CPF
+from validate_docbr import CNPJ
 
 app = Flask(__name__)
 
@@ -19,3 +21,24 @@ def contato():
 @app.route("/produtos")
 def produtos():
     return "<h1>Produtos</h1>"
+
+# página /servicos retorna "Nossos Serviços"
+# página /gerar-cpf retornar um cpf aleatório
+# página /gerar-cnpj retornar cnpj aleatório
+
+# /serviços = pagina serviços
+@app.route("/servicos")
+def servicos():
+    return "<h1>Nossos Serviços</h1>"
+
+@app.route("/gerar-cpf")
+def gerarcpf():
+    cpf = CPF()
+    return cpf.generate(True)
+
+@app.route("/gerar-cnpj")
+def gerarcnpj():
+    cnpj = CNPJ()
+    return cnpj.generate(True)
+
+app.run()
