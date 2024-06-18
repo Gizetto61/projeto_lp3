2c29204befa3fd8292372c724aaf2384de611084
Giovanni Zorzetto Oliveira <gigiozetto@gmail.com>
2024-06-18 11:28:21 -0300

    templates

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from validate_docbr import CPF
 from validate_docbr import CNPJ
 
@@ -10,17 +10,22 @@ app = Flask(__name__)
 # / = home page
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 # /contato = página de contato
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contato.html")
 
 # /produtos = pagina produtos
 @app.route("/produtos")
 def produtos():
-    return "<h1>Produtos</h1>"
+    lista_produtos = [
+        {"nome": "Coca-cola", "descricao": "Mata a sede"},
+        {"nome": "Doritos", "descricao": "Suja a mão"},
+        {"nome": "Red Bull", "descricao": "Te dá Asas"},
+    ]
+    return render_template("produtos.html", produtos = lista_produtos)
 
 # página /servicos retorna "Nossos Serviços"
 # página /gerar-cpf retornar um cpf aleatório
