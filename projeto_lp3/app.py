ca5d1b13ecef168744afc07ef7803c71fac94805
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-20 17:36:55 -0300

    app upgrade

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 
 # rota + função
 
+@app.route("/termos")
+def Termos():
+    return render_template("termos.html")
+
+@app.route("/politica")
+def Politica():
+    return render_template("politica.html")
+
+@app.route("/ultilizacao")
+def Ultilizacao():
+    return render_template("ultilizacao.html")
+
 # / = home page
 @app.route("/")
 def home():
@@ -21,9 +33,9 @@ def contato():
 @app.route("/produtos")
 def produtos():
     lista_produtos = [
-        {"nome": "Coca-cola", "descricao": "Mata a sede"},
-        {"nome": "Doritos", "descricao": "Suja a mão"},
-        {"nome": "Red Bull", "descricao": "Te dá Asas"},
+        {"nome": "Coca-cola", "descricao": "Mata a sede", "imagem": "https://www.callfarma.com.br/_next/image?url=https%3A%2F%2Fd2lakedouw4zad.cloudfront.net%2Fcoca-cola-lata-350ml-74082.png&w=828&q=75"},
+        {"nome": "Doritos", "descricao": "Suja a mão", "imagem": "https://http2.mlstatic.com/D_628495-MLB75957680171_042024-C.jpg"},
+        {"nome": "Red Bull", "descricao": "Te dá Asas", "imagem": "https://d1w7bbx0g2e0q3.cloudfront.net/redbull/products/tmp_JJwqkjWCSUzTgico.png"},
     ]
     return render_template("produtos.html", produtos = lista_produtos)
 
@@ -39,11 +51,13 @@ def servicos():
 @app.route("/gerar-cpf")
 def gerarcpf():
     cpf = CPF()
-    return cpf.generate(True)
+    lista = {cpf.generate(True)}
+    return render_template("CPF.html", cpf = lista)
 
 @app.route("/gerar-cnpj")
 def gerarcnpj():
     cnpj = CNPJ()
-    return cnpj.generate(True)
+    lista1 = {cnpj.generate(True)}
+    return render_template("CNPJ.html", cnpj = lista1)
 
 app.run()
