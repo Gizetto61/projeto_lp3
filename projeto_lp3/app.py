09fb3fb6a9899fc945b136f82fcc4b99cdefa8fb
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-25 11:23:42 -0300

    Formulário_Flask

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from validate_docbr import CPF
 from validate_docbr import CNPJ
+
+lista_produtos = [
+        {"nome": "Coca-cola", "descricao": "Mata a sede", "imagem": "https://www.callfarma.com.br/_next/image?url=https%3A%2F%2Fd2lakedouw4zad.cloudfront.net%2Fcoca-cola-lata-350ml-74082.png&w=828&q=75"},
+        {"nome": "Doritos", "descricao": "Suja a mão", "imagem": "https://http2.mlstatic.com/D_628495-MLB75957680171_042024-C.jpg"},
+        {"nome": "Red Bull", "descricao": "Te dá Asas", "imagem": "https://d1w7bbx0g2e0q3.cloudfront.net/redbull/products/tmp_JJwqkjWCSUzTgico.png"},
+    ]
 
 app = Flask(__name__)
 
@@ -32,11 +38,6 @@ def contato():
 # /produtos = pagina produtos
 @app.route("/produtos")
 def produtos():
-    lista_produtos = [
-        {"nome": "Coca-cola", "descricao": "Mata a sede", "imagem": "https://www.callfarma.com.br/_next/image?url=https%3A%2F%2Fd2lakedouw4zad.cloudfront.net%2Fcoca-cola-lata-350ml-74082.png&w=828&q=75"},
-        {"nome": "Doritos", "descricao": "Suja a mão", "imagem": "https://http2.mlstatic.com/D_628495-MLB75957680171_042024-C.jpg"},
-        {"nome": "Red Bull", "descricao": "Te dá Asas", "imagem": "https://d1w7bbx0g2e0q3.cloudfront.net/redbull/products/tmp_JJwqkjWCSUzTgico.png"},
-    ]
     return render_template("produtos.html", produtos = lista_produtos)
 
 # página /servicos retorna "Nossos Serviços"
@@ -58,6 +59,28 @@ def gerarcpf():
 def gerarcnpj():
     cnpj = CNPJ()
     lista1 = {cnpj.generate(True)}
-    return render_template("CNPJ.html", cnpj = lista1)
+    return render_template("CNPJ.html", cnpj = lista1)deployment.
 
+# GET /produtos/cadastro devolver o form
+deployment.
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# POST /produtos que vai lidar com os dados enviados pelo form
+
+@app.route("/produtos", methods =['POST'])
+def salvar_produto():
+    # Pegando os valores digitados no form
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+
+    # Criar um novo produto (novo dict
+    produto = { "nome": nome, "descricao": descricao, "imagem": "" }
+
+    # Adiciona um produto na lista
+    lista_produtos.append(produto)
+
+    # Devolve o tamplate com o novo produto
+    return render_template("produtos.html", produtos = lista_produtos)
 app.run()
